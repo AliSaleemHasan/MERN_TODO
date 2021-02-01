0fa037d242040fac8e8f61dd2464cac6f48aa64a
=Ali Saleem Hasan <ali1salem1hasan@gmail.com>
2021-02-01 21:22:07 +0200

    adding ./ to app.use static in NODE_ENV if statement

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use((err, req, res, next) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static("./frontend/build"));
 }
 app.listen(port, () => {
   console.log(`Connected Correctly at: http://localhost:${port}`);
